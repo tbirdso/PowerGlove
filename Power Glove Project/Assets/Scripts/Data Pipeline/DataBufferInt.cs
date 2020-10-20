e48abc65bd2466e7bee55eda921ea87b4ef8f2dc
tbirdso <40648863+tbirdso@users.noreply.github.com>
2020-10-20 14:19:04 -0400

    Data Buffer
    Added buffer script to hold incomplete values and notify listeners when complete record is available

---
+++ Power Glove Project/Assets/Scripts/Data Pipeline/DataBufferInt.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+
+// Serializable implementation of generic class
+public class DataBufferInt : DataBuffer<int> { }
+
+// Store asynchronous serial data and signal to the pipeline
+// when a data point or labelled data set is available
+public class DataBuffer<T> : MonoBehaviour
+{
+    #region Members
+    [Tooltip("Most recent complete serial data point")]
+    public T[] record = new T[Defs.NUM_FEATURES];
+    [Tooltip("Most recent complete labelled serial data set")]
+    public T[,] labelledSet = 
+        new T[Defs.NUM_TRAINING_RECORDS, Defs.NUM_TRAINING_COLS];
+
+    public bool isDebug = false;
+
+    [Tooltip("Fires when a new complete record is available to be read.")]
+    public UnityEvent RecordReady = new UnityEvent();
+    [Tooltip("Fires when a new complete dataset is available to be read.")]
+    public UnityEvent LabelledSetReady = new UnityEvent();
+
+    // Serial buffer to store data point, may be incomplete
+    private SortedList<int,T> recordBuf = new SortedList<int,T>();
+    // Buffer to store labelled records, may not be full
+    private T[,] labelSetBuf =
+        new T[Defs.NUM_TRAINING_RECORDS, Defs.NUM_TRAINING_COLS];
+    private int labelSetIndex = 0;
+
+    #endregion
+
+    #region Public Methods
+    // Synchronous method for data receiving object to call to
+    // populate buffer values as serial data becomes available.
+    // Not thread-safe; use synchronous calls from a single
+    // data receiving object.
+    public void AddData(int sensorID, T data)
+    {
+        // Validate input
+        // TODO define IDs to identify each sensor
+        if(sensorID < 0 || sensorID >= Defs.NUM_TRAINING_COLS)
+        {
+            Defs.Debug("Invalid sensor ID: " + sensorID);
+            return;
+        }
+
+        if (isDebug)
+            Defs.Debug("Adding sensor " + sensorID + " value " + data);
+
+        // Update the buffer for the current record, replacing
+        // any previous value recorded for the given sensor 
+        if (recordBuf.ContainsKey(sensorID))
+            recordBuf.Remove(sensorID);
+        recordBuf.Add(sensorID, data);
+
+        // If the buffer has received at least one data point
+        // from every sensor since the last time it was pushed
+        // then push the buffer to the "record" object for
+        // other objects to access on demand
+        if(isRecBufferFull())
+        {
+            record = recordBuf.Values.ToArray();
+
+            for (int index = 0; index < record.Length; index++)
+                labelSetBuf[labelSetIndex, index] = record[index];
+            labelSetIndex++;
+
+            recordBuf.Clear();
+            if (RecordReady != null)
+                RecordReady.Invoke();
+        }
+
+        // If a complete set of training records is recorded in the set buffer
+        // then push it to the on-demand member and create a new buffer
+        if(isSetBufferFull())
+        {
+            labelledSet = labelSetBuf;
+            labelSetBuf = new T[Defs.NUM_TRAINING_RECORDS, Defs.NUM_TRAINING_COLS];
+
+            if (LabelledSetReady != null)
+                LabelledSetReady.Invoke();
+        }
+    }
+
+    #endregion
+
+    #region Private Methods
+    // Determine whether the sorted list has at least one
+    // element for every sensor
+    bool isRecBufferFull()
+    {
+        // TODO handle label column
+        return (recordBuf.Count == Defs.NUM_FEATURES);
+    }
+
+    // Determine whether all rows in the set buffer are filled
+    bool isSetBufferFull()
+    {
+        return (labelSetIndex == Defs.NUM_TRAINING_RECORDS);
+    }
+
+    #endregion
+
+}
---
+++ Power Glove Project/Assets/Scripts/Data Pipeline/TestDataPipeline.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+// Class to test functionality of data preprocessing workflow
+public class TestDataPipeline : MonoBehaviour
+{
+    public GameStateManager manager;
+    public DataBufferInt buffer;
+    public DataPreprocessor preprocessor;
+    public CSVWriter writer;
+    public TFSharpAgent agent;
+
+    private System.Random rand;
+
+    //TODO: where is training data persisted before writing?
+    private int sensorID;
+
+    private int[] realtimeData;
+    private float[] lastScaledRow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rand = new System.Random();
+        buffer.RecordReady.AddListener(OnRecordReady);
+        buffer.LabelledSetReady.AddListener(OnLabelledSetReady);
+    }
+
+    // Update is called once per frame
+    // Generate a random data point for one sensor in each given frame
+    void Update()
+    {
+        if (manager.isTraining)
+        {
+            // Randomly generate and add either a sensor data point or a label
+            if (sensorID > Defs.NUM_TRAINING_RECORDS) sensorID = 0;
+
+            if (sensorID < Defs.NUM_FEATURES)
+                buffer.AddData(sensorID, rand.Next(300, 700));
+            else if (sensorID < Defs.NUM_TRAINING_COLS)
+                buffer.AddData(sensorID, rand.Next(1, 11));
+
+            sensorID++;
+        }
+        else
+        {
+            // Randomly generate and add a sensor data point
+            if (sensorID > Defs.NUM_FEATURES) sensorID = 0;
+
+            buffer.AddData(sensorID, rand.Next(300, 700));
+            sensorID++;
+        }
+    }
+
+    // Handle event when a full record is available. Do preprocessing
+    // for calibration and if allowed run inference
+    void OnRecordReady()
+    {
+        if (!manager.isTraining)
+        {
+            // Updates max/min for each record and returns scaled row
+            lastScaledRow = preprocessor.PreprocessRecord(buffer.record);
+
+            // Run inference if done with calibration,
+            // otherwise discard results
+            if (!manager.isCalibrating)
+            {
+                int label = agent.RunInference(lastScaledRow.ToList());
+                Defs.Debug("Got label " + Defs.LABELS[label]);
+            }
+        }
+    }
+
+    // Handle event when full training data set is ready.
+    // Normalize data and write out to CSV
+    void OnLabelledSetReady()
+    {
+        if (manager.isTraining)
+        {
+            float[,] normalizedData = preprocessor.PreprocessDataSet(buffer.labelledSet);
+            // write out data
+            if (normalizedData != null)
+                writer.WriteData(normalizedData);
+        }
+    }
+}
