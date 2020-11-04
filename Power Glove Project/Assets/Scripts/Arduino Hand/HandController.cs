2993c6083c77d006d5b168ddf2eb02a1790ac84c
DuncanMcCain <duncanfmccain@gmail.com>
2020-11-04 18:16:26 -0500

    Milestone 2 Demo
    Changes made for Milestone 2 Demo

--- Power Glove Project/Assets/Scripts/Arduino Hand/HandController.cs
+++ Power Glove Project/Assets/Scripts/Arduino Hand/HandController.cs
@@ -11,8 +11,9 @@ using System.Threading.Tasks;
 public class HandController : MonoBehaviour
 {
     public Transform wrist;
-    public string portName = "COM6";
+    public string portName = "COM5";
     public int baudRate = 115200;
+    public TFSharpAgent agent;
 
     private Hand hand;
     private SerialPort sp;
@@ -44,29 +45,38 @@ public class HandController : MonoBehaviour
             }
             var glove = (PowerGlove)JsonConvert.DeserializeObject(JsonString, typeof(PowerGlove));
 
+            // thumb_mcp	 thumb_pip	 thumb_hes	 index_mcp	 index_pip	 middle_mcp	 middle_pip
+
+
+
+            var result = agent.RunInference(new List<float>() {glove.thumb_mcp, glove.thumb_pip, glove.thumb_hes, glove.index_mcp, glove.index_pip, glove.middle_mcp, glove.middle_pip});
+
+            Defs.Debug(result.ToString());
+
             //Write data from Json pacakge to the hand
+            hand.thumb.BendJoint(Thumb.MCP, ScaleNum(glove.thumb_mcp));
+            hand.thumb.BendJoint(Thumb.IP, ScaleNum(glove.thumb_pip));
             hand.fingers[Hand.INDEX].BendJoint(Finger.MCP, ScaleNum(glove.index_mcp));
             hand.fingers[Hand.INDEX].BendJoint(Finger.IP, ScaleNum(glove.index_pip));
             hand.fingers[Hand.MIDDLE].BendJoint(Finger.MCP, ScaleNum(glove.middle_mcp));
             hand.fingers[Hand.MIDDLE].BendJoint(Finger.IP, ScaleNum(glove.middle_pip));
-            hand.fingers[Hand.RING].BendJoint(Finger.MCP, ScaleNum(glove.ring_mcp));
-            hand.fingers[Hand.RING].BendJoint(Finger.IP, ScaleNum(glove.ring_pip));
-            hand.fingers[Hand.PINKY].BendJoint(Finger.MCP, ScaleNum(glove.pinky_mcp));
-            hand.fingers[Hand.PINKY].BendJoint(Finger.IP, ScaleNum(glove.pinky_pip));
-            hand.thumb.BendJoint(Thumb.MCP, ScaleNum(glove.thumb_mcp));
-            hand.thumb.BendJoint(Thumb.IP, ScaleNum(glove.thumb_pip));
+            //hand.fingers[Hand.RING].BendJoint(Finger.MCP, ScaleNum(glove.ring_mcp));
+            //hand.fingers[Hand.RING].BendJoint(Finger.IP, ScaleNum(glove.ring_pip));
+            //hand.fingers[Hand.PINKY].BendJoint(Finger.MCP, ScaleNum(glove.pinky_mcp));
+            //hand.fingers[Hand.PINKY].BendJoint(Finger.IP, ScaleNum(glove.pinky_pip));
 
-            //Need to write this function, will probably need to have the glove to test as I go
-            hand.spreadFingers(ScaleNum(glove.index_hes), ScaleNum(glove.ring_hes), ScaleNum(glove.pinky_hes), ScaleNum(glove.thumb_hes));
 
-            //Manually spread a single finger wihtout regard to the other fingers, will be replaced with above function
-            hand.fingers[Hand.INDEX].SpreadFinger(ScaleNum(glove.index_hes));
-            hand.fingers[Hand.RING].SpreadFinger(ScaleNum(glove.ring_hes));
-            hand.fingers[Hand.PINKY].SpreadFinger(ScaleNum(glove.pinky_hes));
-            hand.thumb.SpreadThumb(ScaleNum(glove.thumb_hes));
+            ////Need to write this function, will probably need to have the glove to test as I go
+            //hand.spreadFingers(ScaleNum(glove.index_hes), ScaleNum(glove.ring_hes), ScaleNum(glove.pinky_hes), ScaleNum(glove.thumb_hes));
 
-            //May need to rewrite this function. I cannot tell if it is working with potentiometers, I need the gyroscope in hand to test
-            hand.RotateHand(ScaleNum(glove.pitch), ScaleNum(glove.yaw), ScaleNum(glove.roll));
+            ////Manually spread a single finger wihtout regard to the other fingers, will be replaced with above function
+            //hand.fingers[Hand.INDEX].SpreadFinger(ScaleNum(glove.index_hes));
+            //hand.fingers[Hand.RING].SpreadFinger(ScaleNum(glove.ring_hes));
+            //hand.fingers[Hand.PINKY].SpreadFinger(ScaleNum(glove.pinky_hes));
+            //hand.thumb.SpreadThumb(ScaleNum(glove.thumb_hes));
+
+            ////May need to rewrite this function. I cannot tell if it is working with potentiometers, I need the gyroscope in hand to test
+            //hand.RotateHand(ScaleNum(glove.pitch), ScaleNum(glove.yaw), ScaleNum(glove.roll));
         }
         catch (System.Exception ex)
         {
@@ -109,9 +119,11 @@ public class HandController : MonoBehaviour
 
     private float ScaleNum(int num)
     {
-        //Scale 0 - 1023 to 0 - 90
-        float m = 0.08797f;
-        return m * (float)num;
+        //Scale 140 - 220 to 0 - 90
+        return (float)((num - 220) * (90 / (140 - 220)));
+        //return -(float)((num - 140) * (90 / (220 - 140)));  // meme
+        //float m = 0.08797f;
+        //return m * (float)num;
     }
 }
 
